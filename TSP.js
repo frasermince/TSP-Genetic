b55732058950d6e1e5190623e1a118dcbc5bd2c3
Fraser Mince <bfm09a@acu.edu>
2014-01-30 16:55:29 -0600

    create function to calculate distance between to coordinates

--- TSP.js
+++ TSP.js
@@ -1,6 +1,29 @@
 var fs = require('fs');
 var readline = require('readline');
 
+Number.prototype.toRad = function() {
+   return this * Math.PI / 180;
+}
+
+function distance(one, two){
+	var lat1 = parseFloat(one.lat),
+	lat2 = parseFloat(two.lat),
+	lon1 = parseFloat(one.lon),
+	lon2 = parseFloat(two.lon);
+	var R = 6371;
+	var x1 = lat2-lat1;
+	var x2 = lon2-lon1;
+	var dLat = x1.toRad();
+	var dLon = x2.toRad();
+	var lat1 = lat1.toRad();
+	var lat2 = lat2.toRad();
+
+	var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+	var d = R * c;
+	return d;
+}
+
 var read = readline.createInterface({
 	input: fs.createReadStream(process.argv[2]),
 	output: process.stdout,
